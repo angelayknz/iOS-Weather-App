f62dddea9dec9e09ebff202201a905cd58bb0062
angela <angela_yk@hotmail.com>
2022-10-09 11:31:31 +1300

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Angela Yang on 9/10/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
